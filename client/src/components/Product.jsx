dea7f5713da5a88214900d4d03d5a5160757508c
Vladimir Kirpichev <42208172+djbobah@users.noreply.github.com>
2024-01-29 21:52:52 +0300

    блин не дают поработать :(

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,5 +1,5 @@
 import { React, useRef, useEffect, useState } from "react";
-
+import styles from "./Product.module.css";
 const style = {
   margin: "0 auto",
   border: "solid 1px grey",
@@ -8,8 +8,8 @@ const style = {
 
 const Product = ({ data }) => {
   const canvasRef = useRef();
-  // const [width, setWidth] = useState(0);
-  // const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
   // let width = 0,
   // height = 0;
 
@@ -19,11 +19,13 @@ const Product = ({ data }) => {
     let img = new Image(); // Создаёт новый элемент изображения
     img.src = data.imageSource; // Устанавливает путь
     // console.log(img);/
+    setWidth(img.width / 3);
+    setHeight(img.height / 3);
     img.onload = function () {
       // Здесь вы можете получить ширину и высоту изображения
       // setWidth(img.width);
       // setHeight(img.height);
-      canvasRef.current = { width: img.width, height: img.height };
+      // canvasRef.current = { width: img.width, height: img.height };
       // canvasRef.height = img.height;
       ctx.drawImage(img, 0, 0, img.width / 3, img.height / 3);
     };
@@ -38,14 +40,15 @@ const Product = ({ data }) => {
   }, []);
 
   return (
-    <div style={style}>
-      {data.title}
+    <div className={styles["card"]}>
+      <h3>{data.title}</h3>
       {/* <img src={data.imageSource} alt="" /> */}
       <canvas
+        className={styles["canvas"]}
         id={data.idProduct}
         ref={canvasRef}
-        // width={canvasRef.current.width}
-        // height={canvasRef.current.height}
+        width={width}
+        height={height}
       />
       <div
         className="content"
